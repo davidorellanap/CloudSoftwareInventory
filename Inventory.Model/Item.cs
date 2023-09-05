8b425d5f3924cd98be36b77b0ae0e9d1b121590a
dhpalacios <dhpalacios@SV-2948460>
2023-09-05 13:32:41 -0600

    Add project files.

---
+++ Inventory.Model/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int AvailQuantity { get; set; }
+        public Status Status { get; set; }
+    }
+}
+
+namespace Inventory.Models
+{
+    public enum Status
+    {
+        Ok, Defective
+    }
+}
