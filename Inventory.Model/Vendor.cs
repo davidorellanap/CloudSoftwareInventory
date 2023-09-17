f32a9c2863aa04fd3cc8670d52071a5977e46c86
David Orellana <ia.dhpalacios@ufg.edu.sv>
2023-09-17 10:24:44 -0600

    Changes to the Inventory Model

---
+++ Inventory.Model/Vendor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class Vendor
+    {
+        public int VendorID { get; set; }
+        [Required]
+        public string VendorName { get; set; }
+        [Display(Name = "Vendor Type")]
+        public int VendorTypeId { get; set; }
+        [Display(Name = "Street Addres")]
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        [Display(Name = "Contact Person")]
+        public String ContactPerson { get; set; }
+    }
+}
