6ef0a021832c9b2318d773c20980d470ef5904de
David Orellana <ia.dhpalacios@ufg.edu.sv>
2023-09-19 22:04:34 -0600

    Changes to the Inventory.Web, creation of the SuperAdmin class and adding the logging details of the SuperAdmin class to appsettings json

--- Inventory.Web/Program.cs
+++ Inventory.Web/Program.cs
@@ -1,8 +1,10 @@
+using Inventory.Utility;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.Configure<SuperAdmin>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
